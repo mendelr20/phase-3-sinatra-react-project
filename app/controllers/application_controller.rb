372fae4779f8ab8cea05268efd0a7235e4a39262
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-02-21 22:11:45 -0500

    adding app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+ 
   get "/books" do
     books = Book.all
     books.to_json
